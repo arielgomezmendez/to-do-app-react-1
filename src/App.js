a1a9331744ddc25342486e702981b3a5a87aeddd
Ariel <arielgomezmendez94@gmail.com>
2022-10-05 15:01:38 -0400

    Add button to clean all tasks
    create the function to clean all taks

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import './App.css';
+import "./App.css";
 import Form from "./components/Form/Form";
 import Task from "./components/Task/Task";
 import { useState } from "react";
+import Button from "./components/Button/Button";
 
 function App() {
   const [task, setTask] = useState([]);
+  //Clean all tasks
+  const cleanAll = () => {
+    setTask([])
+  };
 
   return (
     <div className="App">
       <h1>TODO APP</h1>
       <Form task={task} setTask={setTask} />
-      {task.map((task)=>{
-        return(
-          <Task task={task} setTask={setTask} key={task}/>
-        )
+      {task.map((task) => {
+        return <Task task={task} setTask={setTask} key={task} />;
       })}
-      
+      <Button children="Clean all" type="button" onClick={cleanAll} />
     </div>
   );
 }
