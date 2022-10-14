87577d0410351ea3821f6726475de6c0384445ee
Ariel <arielgomezmendez94@gmail.com>
2022-10-13 23:08:31 -0400

    Create ButtonCustomDelete component to delete all tasks

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Form from "./components/Form/Form";
 import TaskLis from "./components/TaskList/TaskLis";
 import Button from "./components/Button/Button";
 import { useState } from "react";
+import ButtonCustomDelete from "./components/ButtonCustomDelete/ButtonCustomDelete";
 
 function App() {
   const [task, setTask] = useState([]);
@@ -16,7 +17,9 @@ function App() {
       <h1>TODO APP</h1>
       <Form task={task} setTask={setTask} />
       <TaskLis task={task} setTask={setTask}/>
-      <Button  children="Clean all" type="button" onClick={cleanAll} buttontype="cleanAll"/>
+      {/*<Button  children="Clean all" type="button" onClick={cleanAll} buttontype="cleanAll"
+      /> */}
+      <ButtonCustomDelete type="button" onClick={cleanAll}/> 
     </div>
   );
 }
---
+++ src/components/ButtonCustomDelete/ButtonCustomDelete.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./ButtonCustomDelete.css";
+
+//The HTML content of this component is copied from  https://uiverse.io/buttons 
+const ButtonCustomDelete = ({onClick, type}) => {
+  return (
+    <div>
+      <button
+      className="cleanAll"
+      onClick={onClick}
+      type={type}
+      >
+        <span className="text">Clean All</span>
+        <span className="icon">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path>
+          </svg>
+        </span>
+      </button>
+    </div>
+  );
+};
+
+export default ButtonCustomDelete;
