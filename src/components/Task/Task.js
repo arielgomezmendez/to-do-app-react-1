3eb7cd99e9a986cabe274d6fc1364e985b3652b0
Ariel <arielgomezmendez94@gmail.com>
2022-10-15 10:45:33 -0400

    Change the delete button for each task

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./Task.css";
 import { FaTrashAlt } from "react-icons/fa";
-
+import { FaPlus } from "react-icons/fa";
 const Task = ({ task, deleteTask }) => {
   return (
     <div className="task">
       <p>{task}</p>
-      <span onClick={() => deleteTask(task)}><FaTrashAlt/></span>
+      <span className="span-deleteTask" onClick={() => deleteTask(task)}>
+        <div className="delteIcon">
+          <FaPlus />
+        </div>
+      </span>
     </div>
   );
 };
